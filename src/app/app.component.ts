f1ef90b385e506898838a631703a2fd2fd45d886
Quentin Gille <xquentin_gille@hotmail.com>
2019-03-30 15:49:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface User {
+	id: number,
+	fullName: string,
+	hobbies: string[],
+	happy: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TestApp';
+	users: User[] = [{
+		id: 1,
+		fullName: "Quentin Gille",
+		happy: true,
+		hobbies: ["Hiking", "Camping"]
+	}, {
+		id: 2,
+		fullName: "Emmy Gze",
+		happy: true,
+		hobbies: ["Knitting", "Camping", "Swimming"]
+	}, {
+		id: 3,
+		fullName: "Lew dark angel",
+		happy: false,
+		hobbies: []
+	}]
 }
